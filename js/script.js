c942fcdf89a953a92a0272ed219cc93698b54a05
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-05-12 16:49:50 +0000

    added my 5-02 repo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: April 22 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// Setting the randomNumberGenerator as function 
+function randomNumberGenerator() {
+  // Setting the variables
+  const positiveNumber = document.getElementById("positive-number").checked
+  let randomNumber
+
+  // Setting the the if and else statment 
+  if (positiveNumber == true) {
+    randomNumber = Math.floor(Math.random() * 6) + 1 // 1 to 6
+  } else {
+    randomNumber = -1 * (Math.floor(Math.random() * 6) + 1); // -1 to -6
+  }
+  // Gettting the reuslt 
+  document.getElementById("result").innerHTML =
+    "<p>The random number is: " + randomNumber + "</p>"
+}
